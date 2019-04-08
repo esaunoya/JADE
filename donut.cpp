67b9ba9b5e925670a120288f0f89a97e6f3faa7a
Esau Noya <esaunoya@outlook.com>
2019-04-08 10:49:24 -0500

    donut.cpp written

---
+++ donut.cpp
@@ -0,0 +1,3 @@
+#include "donut.h"
+
+Donut::Donut(std::string name, double price, double cost, Frosting frosting, Filling filling) : Product{name, price, cost}, _frosting{frosting}, _filling{filling};
