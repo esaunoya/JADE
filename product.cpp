b634b5696a8bc50e2b63621e6260944ce9c116dd
Esau Noya <esaunoya@outlook.com>
2019-04-08 11:01:56 -0500

    product to_string() written

--- product.cpp
+++ product.cpp
@@ -1,4 +1,6 @@
 #include "product.h"
+
+#include <iostream>
 #include <iomanip>
 
 Product::Product(std::string name, double price, double cost) : _name{name}, _price{price}, _cost{cost} {}
@@ -16,8 +18,8 @@ void Product::save(std::ostream& ost) {
 
 std::string Product::name() const {return _name;}
 
-void Product::to_string(){
-
+std::string Product::to_string(){
+  std::cout << product._name << " ($" << std::fixed << std::setprecision(2) << product._price << ')';
 }
 
 //double Product::price() const {return _price;}
