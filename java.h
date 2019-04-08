f0bf3a857639c32962e5f49bee1a301a4d6fdcf4
Esau Noya <esaunoya@outlook.com>
2019-04-08 11:21:27 -0500

    java.h written

---
+++ java.h
@@ -0,0 +1,21 @@
+#ifndef __JAVA_H
+#define __JAVA_H
+
+#include "product.h"
+
+#include <vector>
+
+enum Shot { None, Chocolate, Vanilla, Peppermint, Hazelnut };
+
+class Java : public Product {
+
+public:
+  Java(std::string name, double price, double cost, int darkness);
+  void add_shot(Shot shot);
+
+private:
+  int _darkness;
+  std::vector<Shot> _shots;
+}
+
+#endif
