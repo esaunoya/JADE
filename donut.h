7dfdd905bb150c6d55fe70aac098af51c6d9b0fc
Esau Noya <esaunoya@outlook.com>
2019-04-14 14:31:33 -0500

    fixed donut constructor, added to_string()

--- donut.h
+++ donut.h
@@ -1,21 +1,26 @@
-#ifndef __DONUT_H
-#define __DONUT_H
+#pragma once
 
 #include "product.h"
+#include <vector>
+#include <ostream>
 
-enum Frosting { Unfrosted, Chocolate, Vanilla, Pink };
-enum Filling { Unfilled, Creme, Bavarian, Strawberry };
+enum Frosting {Unfrosted, Chocolate_top, Vanilla_top, Pink_top};
+const std::vector<std::string> frosting_to_string =
+  {"unfrosted", "chocolate", "vanilla", "pink"};
+
+enum Filling {Unfilled, Creme, Bavarian, Strawberry};
+const std::vector<std::string> filling_to_string =
+  {"unfilled", "creme", "Bavarian", "strawberry"};
 
 class Donut : public Product {
 
 public:
-  Donut(std::string name, double price, double cost, Frosting frosting, Filling filling);
+  Donut(std::string name, double price, double cost, Frosting frosting, bool sprinkles, Filling filling);
+  std::string to_string() override;
 
-private:
+protected:
   Frosting _frosting;
   bool _sprinkles;
   Filling _filling;
 
-}
-
-#endif
+};
