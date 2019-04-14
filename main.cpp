d46762d7707f11364654856b1fc9b62b38c8e24b
Esau Noya <esaunoya@outlook.com>
2019-04-14 15:02:11 -0500

    changed application name on create

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include "mainwin.h"
-
 #include <gtkmm.h>
-#include <iostream>
 
 int main(int argc, char *argv[]) {
 
-        auto app = Gtk::Application::create(argc, argv, "edu.uta.cse1325.paint");
+
+        auto app = Gtk::Application::create(argc, argv, "edu.uta.cse1325.jade");
         Mainwin win;
         return app->run(win);
 
